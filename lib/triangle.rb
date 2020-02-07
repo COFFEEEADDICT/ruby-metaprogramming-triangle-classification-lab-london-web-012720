998f20b66c8fdbd2d2bc9457ca0c1e05d09165a6
Bryn <thenazboz@gmail.com>
2020-02-07 12:19:54 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,47 @@
 class Triangle
-  # write code here
+  
+  def initialize(s1 = nil, s2 = nil, s3 = nil)
+    #tri_type = nil
+
+############ errors ###########
+    if s1 <= 0 || s2 <= 0 || s3 <= 0
+       raise TriangleError
+    end
+
+    if s1 == nil || s2 == nil || s3 == nil 
+      raise TriangleError
+    end
+
+    
+    
+    if s1 + s2 <= s3 || s3 + s2 <= s1 || s1 + s3 <= s2
+      raise TriangleError
+    end
+############ errors ###########
+
+
+    if s1 == s2 && s2 == s3 && s3 == s1
+      tri_type = :equilateral
+    elsif s1 == s2 || s2 == s3 || s3 == s1
+      tri_type = :isosceles
+    else 
+      tri_type = :scalene
+    end
+  
+  @tri_type = tri_type
+  end
+#######################
+  def kind
+      @tri_type
+  end
+    
+
+    class TriangleError < StandardError
+          "illegal move bitches"
+    end
+
+  
+
+
+
 end
